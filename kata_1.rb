f850dabd3a1b21a3daa05d12d91a561a8db99805
Jack Shales <jackshalesmusic@gmail.com>
2016-09-28 09:06:10 -0500

    array of words, remove \n

--- kata_1.rb
+++ kata_1.rb
@@ -1,3 +1,11 @@
-file = File.open("wordlist.txt", "r")
-contents = file.read
-puts contents
+file = File.open("wordlist.txt", "rb")
+contents = file.readlines
+clean_contents = []
+file.close
+# contents_ary = contents.split('/n')
+
+contents.each do |word|
+  word.gsub!("\n", "")
+end
+
+p contents
